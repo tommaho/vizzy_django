67860ed9b3a35cd2c8eeb6cce9ee5fd535d94016
tommaho <tom@tommaho.com>
2024-02-29 10:49:12 -0500

    updates

---
+++ vizzy/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class DataSet(models.Model):
+    """Container for uploaded and converted data files."""
+    name = models.CharField(max_length=200)
+    column_count = models.IntegerField()
+    columns = models.IntegerField()
+    row_count = models.BigIntegerField()
+    data = models.JSONField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        """Describe the dataset model."""
+        return f"{self.name}, {self.column_count} columns and {self.row_count} rows."
+    
